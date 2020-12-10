9251c478c7eccdeb60a0f5725f258fec6fb4b376
Pete VS <pete_vs88@me.com>
2020-12-09 22:06:58 -0500

    started writing first test of form

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,3 +6,25 @@ import ContactForm from './ContactForm'
 test('render contact form without errors', () => {
     render(<ContactForm />)
 })
+
+
+
+test('user can fill out and submit form', () => {
+
+    render(<ContactForm />)
+
+    //match for input fields
+    const firstNameInput = screen.getByLabelText
+
+})
+
+//Test first name field exists and can enter text
+//Test last name field exists and can enter text
+//Test email field exists and can enter text
+//Test message field exists and can enter text
+
+//Test that cannot submit if value first name, last name, email not filled
+
+//Test can submit if message blank and remaining fields are full
+
+//Test cannot submit if enter in an incorreclty formatted email
